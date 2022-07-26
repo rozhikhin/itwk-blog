53c10258aaa918224a831b1807939b4c32b5d8c3
Alexander <rozhihin@gmail.com>
2022-07-26 19:06:48 +0300

    Add routes and controllers for Tags

---
+++ app/Http/Controllers/Admin/Blog/Tag/CreateController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Blog\Tag;
+
+use App\Http\Controllers\Controller;
+
+class CreateController extends Controller
+{
+    public function __invoke()
+    {
+        return __METHOD__;
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -18,11 +18,21 @@ Route::get('/', function () {
 });
 
 Route::group(['namespace' => '\App\Http\Controllers\Admin\Blog\Post', 'prefix' => 'admin/blog/post'], function (){
-    Route::get('', 'IndexController')->name('admin.blog.index');
-    Route::get('/{id}', 'ShowController')->name('admin.blog.show')->where(['id' => '[0-9]+']);;
-    Route::get('/create', 'CreateController')->name('admin.blog.create');
+    Route::get('', 'IndexController')->name('admin.blog.post.index');
+    Route::get('/{id}', 'ShowController')->name('admin.blog.post.show')->where(['id' => '[0-9]+']);;
+    Route::get('/create', 'CreateController')->name('admin.blog.post.create');
     Route::post('', 'StoreController')->name('admin.blog.create');
-    Route::get('/{id}/edit', 'EditController')->name('admin.blog.edit')->where(['id' => '[0-9]+']);
-    Route::patch('/{id}', 'UpdateController')->name('admin.blog.update')->where(['id' => '[0-9]+']);
-    Route::delete('/{id}', 'DestroyController')->name('admin.blog.destroy')->where(['id' => '[0-9]+']);
+    Route::get('/{id}/edit', 'EditController')->name('admin.blog.post.edit')->where(['id' => '[0-9]+']);
+    Route::patch('/{id}', 'UpdateController')->name('admin.blog.post.update')->where(['id' => '[0-9]+']);
+    Route::delete('/{id}', 'DestroyController')->name('admin.blog.post.destroy')->where(['id' => '[0-9]+']);
+});
+
+Route::group(['namespace' => '\App\Http\Controllers\Admin\Blog\Tag', 'prefix' => 'admin/blog/tag'], function (){
+    Route::get('', 'IndexController')->name('admin.blog.tag.index');
+    Route::get('/{id}', 'ShowController')->name('admin.blog.tag.show')->where(['id' => '[0-9]+']);;
+    Route::get('/create', 'CreateController')->name('admin.blog.tag.create');
+    Route::post('', 'StoreController')->name('admin.blog.tag.create');
+    Route::get('/{id}/edit', 'EditController')->name('admin.blog.tag.edit')->where(['id' => '[0-9]+']);
+    Route::patch('/{id}', 'UpdateController')->name('admin.blog.tag.update')->where(['id' => '[0-9]+']);
+    Route::delete('/{id}', 'DestroyController')->name('admin.blog.tag.destroy')->where(['id' => '[0-9]+']);
 });
